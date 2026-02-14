cd654f97774c50c3f44668ab08464826e26f6dcd
tatsuya0105 <tatsuya.h080105@gmail.com>
2026-02-14 18:02:17 +0900

    tobu_bus

--- tobubus-notify/dist/notifiers/console.d.ts
+++
@@ -1,5 +0,0 @@
-import type { BusApproach, Notifier } from "../types.js";
-export declare class ConsoleNotifier implements Notifier {
-    notify(approaches: BusApproach[]): Promise<void>;
-}
-//# sourceMappingURL=console.d.ts.map
--- tobubus-notify/dist/scraper.d.ts
+++
@@ -1,4 +0,0 @@
-import type { BusApproach } from "./types.js";
-export declare function fetchApproaches(departureBusstopId: string, arrivalBusstopId: string): Promise<BusApproach[]>;
-export declare function parseApproaches(html: string): BusApproach[];
-//# sourceMappingURL=scraper.d.ts.map
